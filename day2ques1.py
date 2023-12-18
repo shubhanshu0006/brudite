d91c6c8e7f6f5facd887727a1efa49d76936f84a
shubhanshu0006 <152857686+shubhanshu0006@users.noreply.github.com>
2023-12-19 00:18:59 +0530

    Delete day2ques1.py

--- day2ques1.py
+++
@@ -1,7 +0,0 @@
-#https://leetcode.com/problems/single-number-ii/description/
-class Solution(object):
-    def singleNumber(self, nums):
-       unique_set = set(nums)
-       summ = sum(unique_set)*3 - sum(nums)
-       ns = summ//2
-       return ns
